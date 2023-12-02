bdbdf6ca404385e92e202d70a0799a919d003658
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-12-02 06:17:04 +0200

    Add specs for comment and like models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe '#update_comments_count_on_create' do
+    it 'updates the comments count on create' do
+      user = User.create(name: 'John Doe')
+      post = Post.create(author: user, title: 'New post')
+      expect do
+        Comment.create(user:, post:, text: 'New comment')
+      end.to change { post.comments_counter }.by(1)
+    end
+  end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe '#update_likes_count_on_create' do
+    it 'updates the likes count on create' do
+      user = User.create(name: 'John Doe')
+      post = Post.create(author: user, title: 'New post')
+      expect do
+        Like.create(user:, post:)
+      end.to change { post.likes_counter }.by(1)
+    end
+  end
 end
