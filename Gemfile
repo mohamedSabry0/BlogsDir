13c14c0e4fd1e02355993b3406941c6d0f40d4f2
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-12-12 21:27:46 +0200

    Setup devise

--- Gemfile
+++ Gemfile
@@ -79,3 +79,5 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 
 gem 'cssbundling-rails', '~> 1.3'
 gem 'will_paginate', '~> 3.3'
+
+gem "devise", "~> 4.9"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def current_user
-    @current_user = User.first
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
   end
 end
