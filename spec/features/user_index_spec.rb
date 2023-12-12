3425ea6f6002ef26f8dc858224f9427e34c6fbe5
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-12-12 09:48:44 +0200

    Add user index and user show feature specs

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,1 +1,38 @@
 require 'rails_helper'
+
+RSpec.describe 'User Index', type: :feature do
+  before :each do
+    @users = []
+    6.times do |i|
+      @users.push(User.create!(name: "User #{i}", photo: 'https://i.pravatar.cc/150?img=0'))
+    end
+
+    visit users_path
+  end
+
+  it 'shows all users' do
+    @users.each do |user|
+      expect(page).to have_content(user.name)
+    end
+  end
+
+  it 'shows the profile picture for each user' do
+    @users.each do |user|
+      expect(page).to have_css("img[src*='#{user.photo}']")
+    end
+  end
+
+  it 'shows the number of posts each user has written' do
+    @users.each do |user|
+      expect(page).to have_content(user.posts.count)
+    end
+  end
+
+  it 'redirects to the user show page when a user is clicked' do
+    @users.each do |user|
+      click_link user.name
+      expect(page).to have_current_path(user_path(user))
+      visit users_path
+    end
+  end
+end
--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,1 +1,52 @@
 require 'rails_helper'
+
+RSpec.describe 'User Show', type: :feature do
+  before :each do
+    @user = User.create!(name: 'John Doe', photo: 'https://i.pravatar.cc/150?img=0', bio: 'I am a test user.')
+    @posts = []
+    6.times do |i|
+      @posts.push(Post.create!(author: @user, title: "Post #{i}", text: "Body #{i}"))
+    end
+
+    visit user_path(@user)
+  end
+
+  it 'shows the user\'s profile picture' do
+    expect(page).to have_css("img[src*='#{@user.photo}']")
+  end
+
+  it 'shows the user\'s name' do
+    expect(page).to have_content(@user.name)
+  end
+
+  it 'shows the number of posts the user has written' do
+    expect(page).to have_content(@user.posts.count)
+  end
+
+  it 'shows the user\'s bio' do
+    expect(page).to have_content(@user.bio)
+  end
+
+  it 'shows the user\'s most recent 3 posts' do
+    @posts.last(3).reverse.each do |post|
+      expect(page).to have_content(post.title)
+    end
+  end
+
+  it 'shows a button that lets me view all of a user\'s posts' do
+    expect(page).to have_link('See all posts')
+  end
+
+  it 'redirects me to that post\'s show page when I click a user\'s post' do
+    @posts.last(3).reverse.each do |post|
+      click_on post.title
+      expect(page).to have_current_path(user_post_path(@user, post))
+      visit user_path(@user)
+    end
+  end
+
+  it 'redirects me to the user\'s post\'s index page when I click to see all posts' do
+    click_link 'See all posts'
+    expect(page).to have_current_path(user_posts_path(@user))
+  end
+end
