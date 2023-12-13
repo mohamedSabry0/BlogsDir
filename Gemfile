f409fd0d032cd2f18f3de5b5ca93fd0cdfc63dbe
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-13 14:20:47 +0430

    add the confirmations controller

--- Gemfile
+++ Gemfile
@@ -80,4 +80,4 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'cssbundling-rails', '~> 1.3'
 gem 'will_paginate', '~> 3.3'
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,30 +1,13 @@
-# frozen_string_literal: true
-
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-  # def new
-  #   super
-  # end
+  def create
+    user = User.find_by(email: params[:user][:email])
 
-  # POST /resource/confirmation
-  # def create
-  #   super
-  # end
-
-  # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
-
-  # protected
-
-  # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
-
-  # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+    if user && !user.confirmed?
+      user.confirm
+      sign_in(user)
+      redirect_to users_path
+    else
+      redirect_to new_user_confirmation_path
+    end
+  end
 end
